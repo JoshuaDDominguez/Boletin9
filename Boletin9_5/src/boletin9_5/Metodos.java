a51d6248cd504aaa8b15b9b17c3b8bff5114adec
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-23 09:21:06 +0100

    9_6 tabulado

--- Boletin9_5/src/boletin9_5/Metodos.java
+++ Boletin9_5/src/boletin9_5/Metodos.java
@@ -4,61 +4,63 @@
  * and open the template in the editor.
  */
 package boletin9_5;
+
 import java.util.Scanner;
+
 /**
  *
  * @author jdominguezdominguez
  */
 public class Metodos {
-    Scanner sc=new Scanner(System.in);
-    int cant,num,i;
-    
-    public void pedirElementos(){
+
+    Scanner sc = new Scanner(System.in);
+    int cant, num, i;
+
+    public void pedirElementos() {
         System.out.println("De cuanto elementos quiere la serie?");
-        cant= sc.nextInt();
+        cant = sc.nextInt();
     }
-    
-    public void serieA(){
-        num=2;
-        
+
+    public void serieA() {
+        num = 2;
+
         Metodos.this.pedirElementos();
-        cant=cant-1;
-        for(i=0;i<cant;i++){
-            System.out.print(num+" + ");
-            num=num+2;
+        cant = cant - 1;
+        for (i = 0; i < cant; i++) {
+            System.out.print(num + " + ");
+            num = num + 2;
         }
         System.out.print(num);
     }
-    
-    public void serieB(){
-        num=1;
-        int cont=0;
-        
+
+    public void serieB() {
+        num = 1;
+        int cont = 0;
+
         Metodos.this.pedirElementos();
-        for(i=0;i<cant;i++){
-            if(cont==1){
-                System.out.print(" + "+num);
+        for (i = 0; i < cant; i++) {
+            if (cont == 1) {
+                System.out.print(" + " + num);
                 num++;
                 cont--;
-            }
-            else if(cont==0){
-                System.out.print(" - "+num);
+            } else if (cont == 0) {
+                System.out.print(" - " + num);
                 num++;
                 cont++;
             }
         }
     }
-    
-    public void serieC(){
-        int num1=1,num2=1;
-        
+
+    public void serieC() {
+        int num1 = 1, num2 = 1;
+
         Metodos.this.pedirElementos();
-        cant=cant-2;
+        cant = cant - 2;
         System.out.print("0, ");
-        for(i=0;i<cant;i++){
-            System.out.print(num1+", ");
-            num2=num1+num2;
-            num1=num2-num1;
+        for (i = 0; i < cant; i++) {
+            System.out.print(num1 + ", ");
+            num2 = num1 + num2;
+            num1 = num2 - num1;
         }
         System.out.println(num1);
     }
