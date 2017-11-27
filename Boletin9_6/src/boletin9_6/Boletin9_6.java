8551539335181aeb9f35a844228879fbfddd8272
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-27 08:35:29 +0100

    9_6 final

--- Boletin9_6/src/boletin9_6/Boletin9_6.java
+++ Boletin9_6/src/boletin9_6/Boletin9_6.java
@@ -16,6 +16,8 @@ public class Boletin9_6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Metodos obx=new Metodos();
+        obx.clasificarSoldo();
     }
     
 }
