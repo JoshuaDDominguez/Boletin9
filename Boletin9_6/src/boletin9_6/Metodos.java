8c4c859fa275d75cbc1b741719b4a2827941ff96
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-27 08:35:23 +0100

    9_6 final

--- Boletin9_6/src/boletin9_6/Metodos.java
+++ Boletin9_6/src/boletin9_6/Metodos.java
@@ -11,4 +11,33 @@ import java.util.Scanner;
  */
 public class Metodos {
     Scanner sc=new Scanner(System.in);
+    static int soldo=0;
+    float menor=0;
+    int mayor=0,i=0;
+    
+    public void obtenerSoldo(){
+    do{
+        if(soldo<0){
+            System.out.println("Error! Introduzca un soldo positivo\n\n");
+        }
+        System.out.println("Introduzca un sueldo");
+        soldo=sc.nextInt();
+    }while(soldo<0);
+    }
+    
+    public void clasificarSoldo(){
+        do{
+        Metodos.this.obtenerSoldo();
+        if(soldo>1000 && soldo<1750){
+            mayor++;
+            i++;
+        }
+        else if(soldo<1000){
+            menor++;
+            i++;        
+        }
+        }while(soldo!=0);
+        System.out.println(mayor+" cobran entre 1000€ y 1750€. El "+i/100*menor+"% cobran menos de 1000€");
+    }
 }
+
